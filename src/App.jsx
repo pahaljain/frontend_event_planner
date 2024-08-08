747a3b6ef12050b4e114e7d4cfdce409b102ae20
Pahal Jain <77042471+pahaljain@users.noreply.github.com>
2024-08-08 17:50:32 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,11 +18,11 @@ import Contact from "./components/static_pages/Contact";
 import Services from "./components/static_pages/Services";
 
 const App = () => {
-  const [userId, setUserId] = useState("");
-
-  useEffect(() => {
-    setUserId(localStorage.getItem("userId"));
-  }, [userId]);
+  // const [userId, setUserId] = useState("");
+  const user = localStorage.getItem("userId");
+  // useEffect(() => {
+  //   setUserId(localStorage.getItem("userId"));
+  // }, [userId]);
 
   return (
     <Router>
@@ -30,18 +30,15 @@ const App = () => {
       <Routes>
         <Route
           path="/"
-          element={userId ? <Navigate to="/categories" /> : <Login />}
+          element={user ? <Navigate to="/categories" /> : <Login />}
         />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/categories" element={<Categories />} />
         <Route path="/categories/:categoryId" element={<Events />} />
-        <Route
-          path="/events/:eventId"
-          element={<EventDetails userId={userId} />}
-        />
+        <Route path="/events/:eventId" element={<EventDetails />} />
         <Route path="/admin" element={<Admin />} />
-        <Route path="/orders" element={<Orders userId={userId} />} />
+        <Route path="/orders" element={<Orders />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/services" element={<Services />} />
